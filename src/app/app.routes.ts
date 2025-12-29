f5605fa4da51702778da214c1e894218816005ac
Keshav Shukla <kshukla@invession.com>
2025-12-29 16:35:14 +0530

    first commit v18

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'chalkboard',
+          loadComponent: () => import('./board/board.component').then(m => m.BoardComponent)
+    },
+    {
+        path: 'userflow',
+        loadComponent: () => import('./board2/board2.component').then(m => m.Board2Component)
+    },
+];
+
